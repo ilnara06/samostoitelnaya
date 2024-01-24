6b748a1783ffd3a34149ae414623890694a00099
212224 <xch.victor@mail.ru>
2024-01-24 10:58:43 +0300

    Добавьте файлы проекта.

---
+++ sam/Program.cs
@@ -0,0 +1,30 @@
+﻿string input = Console.ReadLine();
+string result = CheckBrackets(input);
+string CheckBrackets(string a)
+    {
+    int count = 0;
+    foreach (char c in a)
+        {
+            if (c == '(')
+            {
+                count++;
+            }
+            else if (c == ')')
+            {
+                count--;
+            }
+        }
+
+        if (count == 0)
+        {
+        Console.WriteLine("true");
+        return "true";
+        }
+        else
+        {
+        Console.WriteLine("false");
+        return "false";
+        }
+       
+    }
+
